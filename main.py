08ca7ee6b6b6d569bfd02e0967b07b95aa01450c
Hugo Tang <hugotang124@gmail.com>
2024-11-07 00:56:09 +0800

    Update args in main.py and start debug on MTGNN

--- main.py
+++ main.py
@@ -87,17 +87,23 @@ def train(data, X, Y, model, criterion, optim, batch_size):
             grad_norm = optim.step()
 
         if iter % 100 == 0:
-            print('iter:{:3d} | loss: {:.3f}'.format(iter,loss.item() / (output.size(0) * data.m)))
+            print("iter:{:3d} | loss: {:.3f}".format(iter,loss.item() / (output.size(0) * data.m)))
         iter += 1
     return total_loss / n_samples
 
 
 def main(args):
 
+    print("Start loading data")
+
     Data = DataLoader(args.data_directory, args.time_interval, train = 0.6, valid = 0.2, device = device, horizon = args.horizon, window = args.seq_in_len, normalize = args.normalize, stationary_check = args.stationarity, noise_removal = args.noise_removal)
 
+    print("Finish preprocessing data")
+    print("Start loading model")
+
+    # Mising kernel set and kernel size
     model = MTGNN(args.gcn_true, args.buildA_true, args.gcn_depth, args.num_nodes,
-                  device, dropout = args.dropout, subgraph_size = args.subgraph_size,
+                  device, kernel_set = [1, 1], kernel_size = 7, dropout = args.dropout, subgraph_size = args.subgraph_size,
                   node_dim = args.node_dim, dilation_exponential = args.dilation_exponential,
                   conv_channels = args.conv_channels, residual_channels = args.residual_channels,
                   skip_channels = args.skip_channels, end_channels = args.end_channels,
@@ -107,9 +113,9 @@ def main(args):
     model = model.to(device)
 
     print(args)
-    print('The recpetive field size is', model.receptive_field)
+    print("The recpetive field size is", model.receptive_field)
     nParams = sum([p.nelement() for p in model.parameters()])
-    print('Number of model parameters is', nParams, flush=True)
+    print("Number of model parameters is", nParams, flush=True)
 
     if args.L1Loss:
         criterion = nn.L1Loss(size_average = False).to(device)
@@ -125,20 +131,20 @@ def main(args):
 
     # At any point you can hit Ctrl + C to break out of training early.
     try:
-        print('begin training')
+        print("begin training")
         for epoch in range(1, args.epochs + 1):
             epoch_start_time = time.time()
             train_loss = train(Data, Data.train[0], Data.train[1], model, criterion, optim, args.batch_size)
             val_loss, val_rae, val_corr = evaluate(Data, Data.valid[0], Data.valid[1], model, evaluateL2, evaluateL1, args.batch_size)
 
             print(
-                '| end of epoch {:3d} | time: {:5.2f}s | train_loss {:5.4f} | valid rse {:5.4f} | valid rae {:5.4f} | valid corr  {:5.4f}'\
+                "| end of epoch {:3d} | time: {:5.2f}s | train_loss {:5.4f} | valid rse {:5.4f} | valid rae {:5.4f} | valid corr  {:5.4f}"\
                     .format(epoch, (time.time() - epoch_start_time), train_loss, val_loss, val_rae, val_corr), flush = True
             )
 
-            # Save the model if the validation loss is the best we've seen so far.
+            # Save the model if the validation loss is the best we"ve seen so far.
             if val_loss < best_val:
-                with open(args.save, 'wb') as f:
+                with open(args.save, "wb") as f:
                     torch.save(model, f)
                 best_val = val_loss
 
@@ -147,11 +153,11 @@ def main(args):
                 print("test rse {:5.4f} | test rae {:5.4f} | test corr {:5.4f}".format(test_acc, test_rae, test_corr), flush = True)
 
     except KeyboardInterrupt:
-        print('-' * 89)
-        print('Exiting from training early')
+        print("-" * 89)
+        print("Exiting from training early")
 
     # Load the best saved model.
-    with open(args.save, 'rb') as f:
+    with open(args.save, "rb") as f:
         model = torch.load(f)
 
     vtest_acc, vtest_rae, vtest_corr = evaluate(Data, Data.valid[0], Data.valid[1], model, evaluateL2, evaluateL1, args.batch_size)
@@ -190,13 +196,13 @@ def run(args):
         rae.append(test_rae)
         corr.append(test_corr)
 
-    print('\n\n')
-    print('10 runs average')
-    print('\n\n')
+    print("\n\n")
+    print("10 runs average")
+    print("\n\n")
     print("valid\trse\trae\tcorr")
     print("mean\t{:5.4f}\t{:5.4f}\t{:5.4f}".format(np.mean(vacc), np.mean(vrae), np.mean(vcorr)))
     print("std\t{:5.4f}\t{:5.4f}\t{:5.4f}".format(np.std(vacc), np.std(vrae), np.std(vcorr)))
-    print('\n\n')
+    print("\n\n")
     print("test\trse\trae\tcorr")
     print("mean\t{:5.4f}\t{:5.4f}\t{:5.4f}".format(np.mean(acc), np.mean(rae), np.mean(corr)))
     print("std\t{:5.4f}\t{:5.4f}\t{:5.4f}".format(np.std(acc), np.std(rae), np.std(corr)))
@@ -204,42 +210,42 @@ def run(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = 'PyTorch Time series forecasting')
-    parser.add_argument('--data-directory', type = str, default = './data/', help = 'location of the data directory')
-    parser.add_argument("--time-interval", type = str, default = "1h", help = "time interval of data")
-    parser.add_argument('--log_interval', type = int, default = 2000, metavar = 'N', help = 'report interval')
-    parser.add_argument('--save', type = str, default = 'model/model.pt', help = 'path to save the final model')
-    parser.add_argument('--optim', type = str, default = 'adam')
-    parser.add_argument('--L1Loss', type = bool, default = True)
-    parser.add_argument('--normalize', type = int, default = 2)
-    parser.add_argument("--stationarity", type = bool, default = False)
-    parser.add_argument("--noise-removal", type = bool , default = False)
-    parser.add_argument('--gcn_true', type = bool, default = True, help = 'whether to add graph convolution layer')
-    parser.add_argument('--buildA_true', type = bool, default = True, help = 'whether to construct adaptive adjacency matrix')
-    parser.add_argument('--gcn_depth', type = int, default = 2, help = 'graph convolution depth')
-    parser.add_argument('--num_nodes', type = int, default = 137, help = 'number of nodes/variables')
-    parser.add_argument('--dropout', type = float, default = 0.3, help ='dropout rate')
-    parser.add_argument('--subgraph_size', type = int, default = 20, help ='k')
-    parser.add_argument('--node_dim', type = int, default = 40, help = 'dim of nodes')
-    parser.add_argument('--dilation_exponential', type = int, default = 2, help = 'dilation exponential')
-    parser.add_argument('--conv_channels', type = int, default = 16, help = 'convolution channels')
-    parser.add_argument('--residual_channels', type = int, default = 16, help = 'residual channels')
-    parser.add_argument('--skip_channels',type = int, default = 32, help = 'skip channels')
-    parser.add_argument('--end_channels', type = int, default = 64, help = 'end channels')
-    parser.add_argument('--in_dim', type = int, default = 1, help = 'inputs dimension')
-    parser.add_argument('--seq_in_len', type = int, default = 24 * 7, help = 'input sequence length')
-    parser.add_argument('--seq_out_len', type = int, default = 1, help = 'output sequence length')
-    parser.add_argument('--horizon', type = int, default = 3)
-    parser.add_argument('--layers', type = int, default = 5, help = 'number of layers')
-    parser.add_argument('--batch_size', type = int, default = 32, help = 'batch size')
-    parser.add_argument('--lr', type = float, default = 0.0001, help = 'learning rate')
-    parser.add_argument('--weight_decay', type = float, default = 0.00001, help = 'weight decay rate')
-    parser.add_argument('--clip', type = int, default = 5, help = 'clip')
-    parser.add_argument('--propalpha', type = float, default = 0.05, help = 'prop alpha')
-    parser.add_argument('--tanhalpha', type = float, default = 3, help = 'tanh alpha')
-    parser.add_argument('--epochs', type = int, default = 1, help = '')
-    parser.add_argument('--num_split', type = int, default = 1,help = 'number of splits for graphs')
-    parser.add_argument('--step_size', type = int, default = 100, help = 'step_size')
+    parser = argparse.ArgumentParser(description = "PyTorch Time series forecasting")
+    parser.add_argument("--data-directory", type = str, default = "./data/", help = "location of the data directory")
+    parser.add_argument("--time_interval", type = str, default = "1h", help = "time interval of data")
+    parser.add_argument("--log_interval", type = int, default = 2000, metavar = "N", help = "report interval")
+    parser.add_argument("--save", type = str, default = "model/model.pt", help = "path to save the final model")
+    parser.add_argument("--optim", type = str, default = "adam")
+    parser.add_argument("--L1Loss", action = "store_true", default = True)
+    parser.add_argument("--normalize", type = int, default = 1)
+    parser.add_argument("--stationarity", action = "store_true", default = False)
+    parser.add_argument("--noise_removal", action = "store_true", default = False)
+    parser.add_argument("--gcn_true", action = "store_true", default = True, help = "whether to add graph convolution layer")
+    parser.add_argument("--buildA_true", action = "store_true", default = True, help = "whether to construct adaptive adjacency matrix")
+    parser.add_argument("--gcn_depth", type = int, default = 2, help = "graph convolution depth")
+    parser.add_argument("--num_nodes", type = int, default = 137, help = "number of nodes/variables")
+    parser.add_argument("--dropout", type = float, default = 0.3, help ="dropout rate")
+    parser.add_argument("--subgraph_size", type = int, default = 20, help ="k")
+    parser.add_argument("--node_dim", type = int, default = 40, help = "dim of nodes")
+    parser.add_argument("--dilation_exponential", type = int, default = 2, help = "dilation exponential")
+    parser.add_argument("--conv_channels", type = int, default = 16, help = "convolution channels")
+    parser.add_argument("--residual_channels", type = int, default = 16, help = "residual channels")
+    parser.add_argument("--skip_channels",type = int, default = 32, help = "skip channels")
+    parser.add_argument("--end_channels", type = int, default = 64, help = "end channels")
+    parser.add_argument("--in_dim", type = int, default = 1, help = "inputs dimension")
+    parser.add_argument("--seq_in_len", type = int, default = 24 * 7, help = "input sequence length")
+    parser.add_argument("--seq_out_len", type = int, default = 1, help = "output sequence length")
+    parser.add_argument("--horizon", type = int, default = 3)
+    parser.add_argument("--layers", type = int, default = 5, help = "number of layers")
+    parser.add_argument("--batch_size", type = int, default = 32, help = "batch size")
+    parser.add_argument("--lr", type = float, default = 0.0001, help = "learning rate")
+    parser.add_argument("--weight_decay", type = float, default = 0.00001, help = "weight decay rate")
+    parser.add_argument("--clip", type = int, default = 5, help = "clip")
+    parser.add_argument("--propalpha", type = float, default = 0.05, help = "prop alpha")
+    parser.add_argument("--tanhalpha", type = float, default = 3, help = "tanh alpha")
+    parser.add_argument("--epochs", type = int, default = 1, help = "")
+    parser.add_argument("--num_split", type = int, default = 1,help = "number of splits for graphs")
+    parser.add_argument("--step_size", type = int, default = 100, help = "step_size")
 
     args = parser.parse_args()
     run(args)
