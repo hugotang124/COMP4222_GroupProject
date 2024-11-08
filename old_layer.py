e927ab093e7c8d363ddf6090137f83915c473b87
Hugo Tang <hugotang124@gmail.com>
2024-11-09 04:16:20 +0800

    Add old_layer and old_net as commented and truncated versions of nnzhan/MTGNN

---
+++ old_layer.py
@@ -0,0 +1,308 @@
+from __future__ import division
+import torch
+import torch.nn as nn
+from torch.nn import init
+import numbers
+import torch.nn.functional as F
+from typing import Optional
+
+
+class nconv(nn.Module):
+    def __init__(self):
+        super(nconv, self).__init__()
+
+    def forward(self, x, A):
+        x = torch.einsum('ncwl,vw->ncvl', (x, A))
+        return x.contiguous()
+
+class dy_nconv(nn.Module):
+    def __init__(self):
+        super(dy_nconv, self).__init__()
+
+    def forward(self, x, A):
+        x = torch.einsum('ncvl,nvwl->ncwl', (x,A))
+        return x.contiguous()
+
+class linear(nn.Module):
+    def __init__(self, c_in: int, c_out: int, bias: bool = True):
+        r"""An implementation of the linear layer, conducting 2D convolution.
+
+        Args:
+            c_in (int): Number of input channels.
+            c_out (int): Number of output channels.
+            bias (bool, optional): Whether to have bias. Default: True.
+        """
+
+        super(linear,self).__init__()
+        self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size = (1, 1), padding = (0, 0), stride = (1, 1), bias = bias)
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+            else:
+                nn.init.uniform_(p)
+
+    def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
+        """
+        Making a forward pass of the linear layer.
+
+        Arg types:
+            * **x** (Pytorch Float Tensor) - Input tensor, with shape (batch_size, c_in, num_nodes, seq_len).
+
+        Return types:
+            * **x** (PyTorch Float Tensor) - Output tensor, with shape (batch_size, c_out, num_nodes, seq_len).
+        """
+
+        return self.mlp(x)
+
+class mixprop(nn.Module):
+    def __init__(self, c_in: int, c_out: int, gdep: int, dropout: float, alpha: float):
+        r"""An implementation of the mix-hop propagation layer.
+
+        Args:
+            c_in (int): Number of input channels.
+            c_out (int): Number of output channels.
+            gdep (int): Depth of graph convolution.
+            dropout (float): Dropout rate.
+            alpha (float): Ratio of retaining the root nodes's original states, a value between 0 and 1.
+        """
+
+        super(mixprop, self).__init__()
+        self.nconv = nconv()
+        self.mlp = linear((gdep + 1) * c_in, c_out)
+        self.gdep = gdep
+        self.dropout = dropout
+        self.alpha = alpha
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+            else:
+                nn.init.uniform_(p)
+
+    def forward(self, x: torch.FloatTensor, adj: torch.FloatTensor) -> torch.FloatTensor:
+        """
+        Making a forward pass of mix-hop propagation.
+
+        Arg types:
+            * **x** (Pytorch Float Tensor) - Input feature Tensor, with shape (batch_size, c_in, num_nodes, seq_len).
+            * **adj** (PyTorch Float Tensor) - Adjacency matrix, with shape (num_nodes, num_nodes).
+
+        Return types:
+            * **ho** (PyTorch Float Tensor) - Hidden representation for all nodes, with shape (batch_size, c_out, num_nodes, seq_len).
+        """
+
+        adj = adj + torch.eye(adj.size(0)).to(x.device)
+        d = adj.sum(1)
+        h = x
+        out = [h]
+        a = adj / d.view(-1, 1)
+
+        for i in range(self.gdep):
+            h = self.alpha*x + (1 - self.alpha) * self.nconv(h, a)
+            out.append(h)
+
+        ho = torch.cat(out, dim = 1)
+        ho = self.mlp(ho)
+        return ho
+
+class dilated_inception(nn.Module):
+    def __init__(self, cin: int, cout: int, dilation_factor: Optional[int] = 2):
+        r"""
+        An implementation of the dilated inception layer.
+        Args:
+            cin (int): Number of input channels.
+            cout (int): Number of output channels.
+            dilated_factor (int, optional): Dilation factor.
+        """    
+
+        super(dilated_inception, self).__init__()
+        self.tconv = nn.ModuleList()
+        self.kernel_set = [2, 3, 6, 7]
+        cout = int(cout / len(self.kernel_set))
+        for kern in self.kernel_set:
+            self.tconv.append(nn.Conv2d(cin, cout, (1, kern), dilation = (1, dilation_factor)))
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+            else:
+                nn.init.uniform_(p)
+
+    def forward(self, input: torch.FloatTensor) -> torch.FloatTensor:
+        """
+        Making a forward pass of dilated inception.
+
+        Arg types:
+            * **input** (Pytorch Float Tensor) - Input feature Tensor, with shape (batch_size, cin, num_nodes, seq_len).
+
+        Return types:
+            * **x** (PyTorch Float Tensor) - Hidden representation for all nodes,
+            with shape (batch_size, c_out, num_nodes, seq_len - 6).
+        """
+
+        x = []
+        for i in range(len(self.kernel_set)):
+            x.append(self.tconv[i](input))
+        for i in range(len(self.kernel_set)):
+            x[i] = x[i][..., -x[-1].size(3):]
+        x = torch.cat(x, dim = 1)
+        return x
+
+
+class graph_constructor(nn.Module):
+    def __init__(self, nnodes: int, k: int, dim: int, device: str, alpha: Optional[float] = 3, static_feat: Optional[int] = None):
+        r"""An implementation of the graph learning layer to construct an adjacency matrix.
+
+        Args:
+            nnodes (int): Number of nodes in the graph.
+            k (int): Number of largest values to consider in constructing the neighbourhood of a node (pick the "nearest" k nodes).
+            device (str): Device used for model training
+            dim (int): Dimension of the node embedding.
+            alpha (float, optional): Tanh alpha for generating adjacency matrix, alpha controls the saturation rate
+            static_feat (int, optional): Static feature dimension, default None.
+        """
+
+        super(graph_constructor, self).__init__()
+        self.nnodes = nnodes
+        if static_feat is not None:
+            xd = static_feat.shape[1]
+            self.lin1 = nn.Linear(xd, dim)
+            self.lin2 = nn.Linear(xd, dim)
+        else:
+            self.emb1 = nn.Embedding(nnodes, dim)
+            self.emb2 = nn.Embedding(nnodes, dim)
+            self.lin1 = nn.Linear(dim,dim)
+            self.lin2 = nn.Linear(dim,dim)
+
+        self.device = device
+        self.k = k
+        self.dim = dim
+        self.alpha = alpha
+        self.static_feat = static_feat
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+            else:
+                nn.init.uniform_(p)
+
+    def forward(self, idx: torch.LongTensor) -> torch.FloatTensor:
+        """
+        Making a forward pass to construct an adjacency matrix from node embeddings.
+
+        Arg types:
+            * **idx** (Pytorch Long Tensor) - Input indices, a permutation of the number of nodes, default None (no permutation).
+        Return types:
+            * **adj** (PyTorch Float Tensor) - Adjacency matrix constructed from node embeddings.
+        """
+
+        if self.static_feat is None:
+            nodevec1 = self.emb1(idx)
+            nodevec2 = self.emb2(idx)
+        else:
+            nodevec1 = self.static_feat[idx,:]
+            nodevec2 = nodevec1
+
+        nodevec1 = torch.tanh(self.alpha * self.lin1(nodevec1))
+        nodevec2 = torch.tanh(self.alpha * self.lin2(nodevec2))
+
+        a = torch.mm(nodevec1, nodevec2.transpose(1,0)) - torch.mm(nodevec2, nodevec1.transpose(1, 0))
+        adj = F.relu(torch.tanh(self.alpha * a))
+        mask = torch.zeros(idx.size(0), idx.size(0)).to(self.device)
+        mask.fill_(float('0'))
+        s1, t1 = (adj + torch.rand_like(adj) * 0.01).topk(self.k, 1)
+        mask.scatter_(1, t1, s1.fill_(1))
+        adj = adj * mask
+        return adj
+
+    def fullA(self, idx: torch.LongTensor) -> torch.FloatTensor:
+        '''
+        Construct full adjacency matrix from node embeddings.
+
+        Arg types:
+            * **idx** (Pytorch Long Tensor) - Input indices, a permutation of the number of nodes, default None (no permutation).
+        Return types:
+            * **adj** (PyTorch Float Tensor) - Adjacency matrix constructed from node embeddings.
+
+        '''
+
+        if self.static_feat is None:
+            nodevec1 = self.emb1(idx)
+            nodevec2 = self.emb2(idx)
+        else:
+            nodevec1 = self.static_feat[idx,:]
+            nodevec2 = nodevec1
+
+        nodevec1 = torch.tanh(self.alpha * self.lin1(nodevec1))
+        nodevec2 = torch.tanh(self.alpha * self.lin2(nodevec2))
+
+        a = torch.mm(nodevec1, nodevec2.transpose(1,0)) - torch.mm(nodevec2, nodevec1.transpose(1, 0))
+        adj = F.relu(torch.tanh(self.alpha * a))
+        return adj
+
+class layernorm(nn.Module):
+    __constants__ = ['normalized_shape', 'weight', 'bias', 'eps', 'elementwise_affine']
+
+    def __init__(self, normalized_shape: int, eps: Optional[float] = 1e-5, elementwise_affine: bool = True):
+        r"""An implementation of the layer normalization layer.
+
+        Args:
+            normalized_shape (int): Input shape from an expected input of size.
+            eps (float, optional): Value added to the denominator for numerical stability. Default: 1e-5.
+            elementwise_affine (bool, optional): Whether to conduct elementwise affine transformation or not. Default: True.
+        """
+
+        super(layernorm, self).__init__()
+        if isinstance(normalized_shape, numbers.Integral):
+            normalized_shape = (normalized_shape, )
+        self.normalized_shape = tuple(normalized_shape)
+        self.eps = eps
+        self.elementwise_affine = elementwise_affine
+        if self.elementwise_affine:
+            self.weight = nn.Parameter(torch.Tensor(*normalized_shape))
+            self.bias = nn.Parameter(torch.Tensor(*normalized_shape))
+        else:
+            self.register_parameter('weight', None)
+            self.register_parameter('bias', None)
+        self.reset_parameters()
+
+
+    def reset_parameters(self):
+        if self.elementwise_affine:
+            init.ones_(self.weight)
+            init.zeros_(self.bias)
+
+    def forward(self, input: torch.FloatTensor, idx: torch.FloatTensor) -> torch.FloatTensor:
+        """
+        Making a forward pass of layer normalization.
+
+        Arg types:
+            * **input** (Pytorch Float Tensor) - Input tensor,
+                with shape (batch_size, feature_dim, num_nodes, seq_len).
+            * **idx** (Pytorch Long Tensor) - Input indices.
+
+        Return types:
+            * **X** (PyTorch Float Tensor) - Output tensor,
+                with shape (batch_size, feature_dim, num_nodes, seq_len).
+        """
+
+        if self.elementwise_affine:
+            return F.layer_norm(input, tuple(input.shape[1:]), self.weight[:,idx,:], self.bias[:,idx,:], self.eps)
+        else:
+            return F.layer_norm(input, tuple(input.shape[1:]), self.weight, self.bias, self.eps)
+
+    def extra_repr(self):
+        return '{normalized_shape}, eps={eps}, ' \
+            'elementwise_affine={elementwise_affine}'.format(**self.__dict__)
---
+++ old_net.py
@@ -0,0 +1,155 @@
+from old_layer import *
+from typing import Optional
+
+class gtnet(nn.Module):
+    def __init__(self, gcn_true: bool, buildA_true: bool, gcn_depth: int, num_nodes: int, device: str, dropout: float, subgraph_size: int, node_dim: int, dilation_exponential: int, 
+                 conv_channels: int, residual_channels: int, skip_channels: int, end_channels: int, seq_length: int, in_dim: int, out_dim: int, layers: int, propalpha: float, tanhalpha: float, 
+                 layer_norm_affline = True, predefined_A: torch.FloatTensor = None, static_feat: Optional[int] = None):
+
+        r"""An implementation of the Multivariate Time Series Forecasting Graph Neural Networks.
+        For details see this paper: `"Connecting the Dots: Multivariate Time Series Forecasting with Graph Neural Networks."
+        <https://arxiv.org/pdf/2005.11650.pdf>`_
+
+        Args:
+            gcn_true (bool): Whether to add graph convolution layer.
+            build_adj (bool): Whether to construct adaptive adjacency matrix.
+            gcn_depth (int): Graph convolution depth.
+            num_nodes (int): Number of nodes in the graph.
+            kernel_set (list of int): List of kernel sizes.
+            kernel_size (int): Size of kernel for convolution, to calculate receptive field size.
+            dropout (float): Droupout rate.
+            subgraph_size (int): Size of subgraph.
+            node_dim (int): Dimension of nodes.
+            dilation_exponential (int): Dilation exponential.
+            conv_channels (int): Convolution channels.
+            residual_channels (int): Residual channels.
+            skip_channels (int): Skip channels.
+            end_channels (int): End channels.
+            seq_length (int): Length of input sequence.
+            in_dim (int): Input dimension.
+            out_dim (int): Output dimension.
+            layers (int): Number of layers.
+            propalpha (float): Prop alpha, ratio of retaining the root nodes's original states in mix-hop propagation, a value between 0 and 1.
+            tanhalpha (float): Tanh alpha for generating adjacency matrix, alpha controls the saturation rate.
+            layer_norm_affline (bool): Whether to do elementwise affine in Layer Normalization.
+            predefined_adj (FloatTensor): Predefined adjacency matrix
+            static_feat (int, optional): Static feature dimension, default None.
+        """
+
+        super(gtnet, self).__init__()
+        self.gcn_true = gcn_true
+        self.buildA_true = buildA_true
+        self.num_nodes = num_nodes
+        self.dropout = dropout
+        self.predefined_A = predefined_A
+        self.filter_convs = nn.ModuleList()
+        self.gate_convs = nn.ModuleList()
+        self.residual_convs = nn.ModuleList()
+        self.skip_convs = nn.ModuleList()
+        self.gconv1 = nn.ModuleList()
+        self.gconv2 = nn.ModuleList()
+        self.norm = nn.ModuleList()
+        self.start_conv = nn.Conv2d(in_channels = in_dim, out_channels = residual_channels, kernel_size = (1, 1))
+        self.gc = graph_constructor(num_nodes, subgraph_size, node_dim, device, alpha = tanhalpha, static_feat = static_feat)
+
+        self.seq_length = seq_length
+        kernel_size = 7
+
+        if dilation_exponential > 1:
+            self._receptive_field = int(1 + (kernel_size - 1) * (dilation_exponential ** layers - 1) / (dilation_exponential - 1))
+        else:
+            self._receptive_field = layers * (kernel_size - 1) + 1
+
+
+        rf_size_i = 1
+        new_dilation = 1
+
+        for j in range(1, layers + 1):
+            if dilation_exponential > 1:
+                rf_size_j = int(rf_size_i + (kernel_size - 1) * (dilation_exponential ** j - 1) / (dilation_exponential - 1))
+            else:
+                rf_size_j = rf_size_i + j * (kernel_size - 1)
+
+            self.filter_convs.append(dilated_inception(residual_channels, conv_channels, dilation_factor = new_dilation))
+            self.gate_convs.append(dilated_inception(residual_channels, conv_channels, dilation_factor = new_dilation))
+            self.residual_convs.append(nn.Conv2d(in_channels = conv_channels, out_channels = residual_channels, kernel_size = (1, 1)))
+            if self.seq_length > self._receptive_field:
+                self.skip_convs.append(nn.Conv2d(in_channels = conv_channels, out_channels = skip_channels, kernel_size = (1, self.seq_length - rf_size_j + 1)))
+            else:
+                self.skip_convs.append(nn.Conv2d(in_channels = conv_channels, out_channels = skip_channels, kernel_size = (1, self._receptive_field - rf_size_j + 1)))
+
+            if self.gcn_true:
+                self.gconv1.append(mixprop(conv_channels, residual_channels, gcn_depth, dropout, propalpha))
+                self.gconv2.append(mixprop(conv_channels, residual_channels, gcn_depth, dropout, propalpha))
+
+            if self.seq_length>self._receptive_field:
+                self.norm.append(layernorm((residual_channels, num_nodes, self.seq_length - rf_size_j + 1), elementwise_affine = layer_norm_affline))
+            else:
+                self.norm.append(layernorm((residual_channels, num_nodes, self._receptive_field - rf_size_j + 1), elementwise_affine = layer_norm_affline))
+
+            new_dilation *= dilation_exponential
+
+        self.layers = layers
+        self.end_conv_1 = nn.Conv2d(in_channels = skip_channels, out_channels = end_channels, kernel_size = (1, 1), bias = True)
+        self.end_conv_2 = nn.Conv2d(in_channels = end_channels, out_channels = out_dim, kernel_size = (1, 1), bias = True)
+        if self.seq_length > self._receptive_field:
+            self.skip0 = nn.Conv2d(in_channels = in_dim, out_channels = skip_channels, kernel_size = (1, self.seq_length), bias = True)
+            self.skipE = nn.Conv2d(in_channels = residual_channels, out_channels = skip_channels, kernel_size = (1, self.seq_length - self._receptive_field + 1), bias = True)
+
+        else:
+            self.skip0 = nn.Conv2d(in_channels = in_dim, out_channels = skip_channels, kernel_size = (1, self._receptive_field), bias = True)
+            self.skipE = nn.Conv2d(in_channels = residual_channels, out_channels = skip_channels, kernel_size = (1, 1), bias = True)
+
+        self.idx = torch.arange(self.num_nodes).to(device)
+
+
+    def forward(self, input: torch.FloatTensor, idx: Optional[torch.LongTensor] = None) -> torch.FloatTensor:
+        """
+        Making a forward pass of MTGNN.
+
+        Arg types:
+            * **input** (PyTorch FloatTensor) - Input sequence, with shape (batch_size, in_dim, num_nodes, seq_len).
+            * **idx** (Pytorch LongTensor, optional) - Input indices, a permutation of the num_nodes, default None (no permutation).
+
+        Return types:
+            * **x** (PyTorch FloatTensor) - Output sequence for prediction, with shape (batch_size, seq_len, num_nodes, 1).
+        """
+
+        seq_len = input.size(3)
+        assert seq_len == self.seq_length, 'input sequence length not equal to preset sequence length'
+
+        if self.seq_length < self._receptive_field:
+            input = nn.functional.pad(input, (self._receptive_field - self.seq_length, 0, 0, 0))
+
+
+        if self.gcn_true:
+            if self.buildA_true:
+                adp = self.gc(self.idx) if idx is None else self.gc(idx)
+            else:
+                adp = self.predefined_A
+
+        x = self.start_conv(input)
+        skip = self.skip0(F.dropout(input, self.dropout, training = self.training))
+
+        for i in range(self.layers):
+            residual = x
+            filter = self.filter_convs[i](x)
+            filter = torch.tanh(filter)
+            gate = self.gate_convs[i](x)
+            gate = torch.sigmoid(gate)
+            x = filter * gate
+            x = F.dropout(x, self.dropout, training = self.training)
+            s = x
+            s = self.skip_convs[i](s)
+            skip = s + skip
+
+            x = self.gconv1[i](x, adp) + self.gconv2[i](x, adp.transpose(1, 0)) if self.gcn_true else self.residual_convs[i](x)
+            x = x + residual[:, :, :, -x.size(3):]
+            x = self.norm[i](x, self.idx) if idx is None else self.norm[i](x, idx)
+
+        skip = self.skipE(x) + skip
+        x = F.relu(skip)
+        x = F.relu(self.end_conv_1(x))
+        x = self.end_conv_2(x)
+
+        return x
