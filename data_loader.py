Solve NewMTGNN bug with scaling

data_loader.py
@@ -1,5 +1,5 @@
 import pickle
-from typing import List
+from typing import List, Optional
 import numpy as np
 import os
 import glob
@@ -19,8 +19,92 @@ from ta.trend import EMAIndicator, MACD
 from ta.volume import VolumeWeightedAveragePrice
 
 
-def normal_std(x):
+def normal_std(x: torch.FloatTensor) -> float:
     return x.std() * np.sqrt((len(x) - 1.)/(len(x)))
+
+class StandardScaler():
+    def __init__(self, mean: torch.FloatTensor, std: torch.FloatTensor, device: str):
+        """
+        Standardisation scaler for data
+
+        Arg types:
+            **mean** (PyTorch Float Tensor): Mean matrix of data
+            **std** (PyTorch Float Tensor): Standard deviation matrix of data
+            device (str): Device used for model training
+        """
+        self.mean = mean.to(device)
+        self.std = std.to(device)
+
+    def transform(self, data: torch.FloatTensor) -> torch.FloatTensor:
+        '''
+        Transformation with standardisation
+
+        Arg types:
+            **data*8 (PyTorch Float Tensor): Data.
+
+        Return types:
+            **X** (PyTorch Float Tensor): Standardised Tensor.
+        '''
+        return (data - self.mean) / self.std
+
+    def inverse_transform(self, data: torch.FloatTensor, id: torch.Tensor = None) -> torch.FloatTensor:
+        '''
+        Inverse transformation of standardisation regarding output
+
+        Arg types:
+            data (PyTorch Float Tensor): Data
+            id (PyTorch Tensor):  Input indices, a permutation of the num_nodes, default None.
+
+        Return types:
+            **X** (PyTorch Float Tensor): Standardised Tensor
+        '''
+
+        if id is None:
+            return (data * self.std) + self.mean
+
+        return (data * self.std[:, id]) + self.mean[:, id]
+
+class MaxScaler():
+    def __init__(self, max: torch.FloatTensor, device: str):
+        """
+        Max value standardisation scaler for data
+
+        Arg types:
+            **max** (PyTorch Float Tensor): Max matrix of data
+            device (str): Device used for model training
+        """
+
+        self.max = max.unsqueeze(1).to(device)
+
+    def transform(self, data: torch.FloatTensor) -> torch.FloatTensor:
+        '''
+        Transformation with max value standardisation
+
+        Arg types:
+            **data*8 (PyTorch Float Tensor): Data.
+
+        Return types:
+            **X** (PyTorch Float Tensor): Standardised Tensor.
+        '''
+
+        return data / self.max
+
+    def inverse_transform(self, data: torch.FloatTensor, id: Optional[torch.Tensor] = None) -> torch.FloatTensor:
+        '''
+        Inverse transformation of standardisation regarding output
+
+        Arg types:
+            data (PyTorch Float Tensor): Data
+            id (PyTorch Tensor):  Input indices, a permutation of the num_nodes, default None.
+
+        Return types:
+            **X** (PyTorch Float Tensor): Standardised Tensor
+        '''
+
+        if id is None:
+            return data * self.max
+
+        return data * self.max[:, id]
 
 
 class DataLoader(object):
@@ -43,10 +127,13 @@ class DataLoader(object):
             data.columns = [f"{currency.lower()}_{x}" for x in data.columns]
 
         self.raw_data = pd.concat(currencies_data.values(), axis = 1)
-        self._build_data_features()
         self.raw_data = self.raw_data.loc[self.start_time:]
         self.raw_data.dropna(axis = 1, inplace = True)
+        self.currencies = list(set(map(lambda x: x.split("_")[0], self.raw_data.columns)))
+        self._build_data_features()
 
+        # Save memory
+        currencies_data.clear()
 
         # Stationarity checking
         if stationary_check:
@@ -60,30 +147,20 @@ class DataLoader(object):
         else:
             self.data = self.raw_data.to_numpy()
 
-
         self.n, self.m = self.data.shape
-
+        print(f"There are {len(self.currencies)} currencies involved in the data.")
+        print(f"Dataframe Shape: {self.data.shape}")
 
         self.P = window
         self.h = horizon
         self.normalize = normalize
-        self.scale = np.ones([3, self.m])
         self._split(train, valid)
-        self._normalized(normalize)
-
-
-
-        self.scale = torch.from_numpy(self.scale).float()
-        tmp = self.test[1] * self.scale[-1, :].expand(self.test[1].size(0), self.m)
-
-        self.scale = self.scale.to(device)
-
-        self.scale = Variable(self.scale)
-
-        self.rse = normal_std(tmp)
-        self.rae = torch.mean(torch.abs(tmp - torch.mean(tmp)))
-
         self.device = device
+
+
+        self.rse = normal_std(self.test[1])
+        self.rae = torch.mean(torch.abs(self.test[1] - torch.mean(self.test[1])))
+
 
     def _build_currency_features(self, currency: str, data: pd.DataFrame):
         '''
@@ -95,17 +172,17 @@ class DataLoader(object):
 
 
         windows = [5, 10, 20]
-        
+
         data['volume_quote_ratio'] = data['volume'] / data['quote_volume']
         data['buy_sell_volume_ratio'] = data['buy_base_vol'] / data['volume']
         data['buy_sell_quote_ratio'] = data['buy_quote_vol'] / data['quote_volume']
-        
-        
+
+
         # Stochastic Oscillator
         stoch = StochasticOscillator(high = data['high'], low = data['low'], close = data['close'])
         data['stoch_k'] = stoch.stoch()
         data['stoch_d'] = stoch.stoch_signal()
-        
+
         # VWAP
         vwap = VolumeWeightedAveragePrice(high = data['high'], low = data['low'],
                                          close = data['close'], volume = data['volume'])
@@ -114,19 +191,19 @@ class DataLoader(object):
         for window in windows:
             # Parkinson Volatility
             data[f'parkinson_vol_{window}'] = np.sqrt(
-                (1.0 / (4.0 * np.log(2.0))) * 
+                (1.0 / (4.0 * np.log(2.0))) *
                 (np.log(data['high'] / data['low']) ** 2).rolling(window).mean()
             )
-            
+
             # Garman-Klass Volatility
             data[f'garman_klass_vol_{window}'] = np.sqrt(
                 (0.5 * np.log(data['high'] / data['low']) ** 2) -
                 (2.0 * np.log(2.0) - 1.0) * (np.log(data['close'] / data['open']) ** 2)
             ).rolling(window).mean()
-        
+
         data['avg_trade_size'] = data['volume'] / data['trades']
         data['avg_trade_quote_size'] = data['quote_volume'] / data['trades']
-        
+
 
         data.drop(columns = ["open", "high", "low"], inplace = True)
 
@@ -178,30 +255,36 @@ class DataLoader(object):
 
         return np.convolve(signal, np.ones(window) / window, mode = 'same')
 
-    def _normalized(self, normalize: int = 1):
+    def _normalized(self, X, Y, normalize: int = 1):
         '''
         Normalize data with one of the multiple methods
         Args:
             normalize (int): Normalization mode
         '''
+        if (normalize == 0):
+            data = data
+            X_scaler = StandardScaler(mean = 0, std = 1, device = self.device)
+            Y_scaler = StandardScaler(mean = 0, std = 1, device = self.device)
 
-        for i, (X, Y) in enumerate([self.train, self.valid, self.test]):
-            if (normalize == 0):
-                data = data
+        elif (normalize == 1):
+            # Standardisation
 
-            elif (normalize == 1):
-                # Standardisation
+            X_scaler = StandardScaler(mean = X.mean(dim = 0, keepdim = True),  std = X.std(dim = 0, keepdim = True), device = self.device)
+            X = X_scaler.transform(X)
+            del X_scaler
 
-                means = X.mean(dim = 1, keepdim = True)
-                stds = X.std(dim = 1, keepdim = True)
-                X = (X - means) / stds
+            Y_scaler = StandardScaler(mean = Y.mean(dim = 0, keepdim = True),  std = Y.std(dim = 0, keepdim = True), device = self.device)
 
-            elif (normalize == 2):
-                # Normalization based on the maximum of each column
-                # Need adjustment in the evaluation and training function regarding error calculation
+        elif (normalize == 2):
+            # Normalization based on the maximum of each column
 
-                self.scale[i, :] = X.max(dim = 1).values
-                X = X / X.max(dim = 1, keepdim = True)
+            X_scaler = MaxScaler(max = X.max(dim = 0).values, device = self.device)
+            X = X_scaler.transform(X)
+            del X_scaler
+
+            Y_scaler = MaxScaler(max = Y.max(dim = 0).values, device = self.device)
+
+        return X, Y, Y_scaler
 
     def _split(self, train: float, valid: float):
         '''
@@ -232,9 +315,10 @@ class DataLoader(object):
             start = end - self.P
             X[i, :, :] = torch.from_numpy(self.data[start:end, :])
             Y[i, :] = torch.from_numpy(self.raw_data.iloc[idx_set[i], :].to_numpy())
+
         return [X, Y]
 
-    def get_batches(self, inputs, targets, batch_size, shuffle=True):
+    def get_batches(self, inputs, targets, batch_size, shuffle = True):
         length = len(inputs)
         if shuffle:
             index = torch.randperm(length)
@@ -245,11 +329,14 @@ class DataLoader(object):
         while start_idx < length:
             end_idx = min(length, start_idx + batch_size)
             excerpt = index[start_idx:end_idx]
-            
+
             # Ensure inputs and targets are tensors
             X = inputs[excerpt].to(self.device)
             Y = targets[excerpt].to(self.device)
-            
-            yield X, Y  # No need for Variable
+            X, Y, Y_scaler = self._normalized(X, Y, self.normalize)
+            X = X.to(self.device)
+            Y = Y.to(self.device)
+
+            yield X, Y, Y_scaler # No need for Variable
 
             start_idx += batch_size

main.py
@@ -1,6 +1,7 @@
 import argparse
 import math
 import time
+import os
 
 import torch
 import torch.nn as nn
@@ -13,7 +14,6 @@ from optimizer import Optim
 from model import BaselineModels
 import torch.optim as optimizing
 
-
 def evaluate(data, X, Y, model, evaluateL2, evaluateL1, batch_size):
     model.eval()
     total_loss = 0
@@ -21,16 +21,13 @@ def evaluate(data, X, Y, model, evaluateL2, evaluateL1, batch_size):
     n_samples = 0
     predict = None
     test = None
-    iter = 0
 
-    for X, Y in data.get_batches(X, Y, batch_size, False):
+    for X, Y, Y_scaler in data.get_batches(X, Y, batch_size, False):
         X = torch.unsqueeze(X, dim = 1)
         X = X.transpose(2, 3)
         with torch.no_grad():
             output = model(X)
         output = torch.squeeze(output)
-        Y = Y.unsqueeze(2) 
-        Y = Y.expand(-1, -1, model._receptive_field) 
         if len(output.shape) == 1:
             output = output.unsqueeze(dim = 0)
         if predict is None:
@@ -40,32 +37,23 @@ def evaluate(data, X, Y, model, evaluateL2, evaluateL1, batch_size):
             predict = torch.cat((predict, output))
             test = torch.cat((test, Y))
 
-        if output.size(0) > 2:
-            scale = data.scale.expand(output.size(0), data.m)
+        output = Y_scaler.inverse_transform(output)
 
-        scale = torch.squeeze(scale)
-        scale = scale.unsqueeze(2) 
-        scale = scale.expand(-1, -1, model._receptive_field)
-        total_loss += evaluateL2(output * scale, Y * scale).item()
-        total_loss_l1 += evaluateL1(output * scale, Y * scale).item()
+        total_loss += evaluateL2(output, Y).item()
+        total_loss_l1 += evaluateL1(output, Y).item()
         n_samples += (output.size(0) * data.m)
-
-        if iter > 201:
-            break
-
-        iter += 1
 
     rse = math.sqrt(total_loss / n_samples) / data.rse
     rae = (total_loss_l1 / n_samples) / data.rae
 
     predict = predict.data.cpu().numpy()
     Ytest = test.data.cpu().numpy()
-    sigma_p = (predict).std(axis = (0,2))
-    sigma_g = (Ytest).std(axis = (0,2))
-    mean_p = predict.mean(axis = (0))
-    mean_g = Ytest.mean(axis = (0))
+    sigma_p = (predict).std(axis = 0)
+    sigma_g = (Ytest).std(axis = 0)
+    mean_p = predict.mean(axis = 0)
+    mean_g = Ytest.mean(axis = 0)
     index = (sigma_g != 0)
-    correlation = ((predict - mean_p) * (Ytest - mean_g)).mean(axis = (0,2)) / (sigma_p * sigma_g)
+    correlation = ((predict - mean_p) * (Ytest - mean_g)).mean(axis = 0) / (sigma_p * sigma_g)
     correlation = (correlation[index]).mean()
     return rse, rae, correlation
 
@@ -75,13 +63,16 @@ def train(data, X, Y, model, criterion, optim, batch_size):
     total_loss = 0
     n_samples = 0
     iter = 0
-    for X, Y in data.get_batches(X, Y, batch_size, True):
+
+    track_time = time.time()
+
+    for X, Y, Y_scaler in data.get_batches(X, Y, batch_size, True):
         model.zero_grad()
         X = torch.unsqueeze(X, dim = 1)
         X = X.transpose(2, 3)
         if iter % args.step_size == 0:
-            perm = np.random.permutation(range(args.num_nodes))
-        num_sub = int(args.num_nodes / args.num_split)
+            perm = np.random.permutation(range(num_nodes))
+        num_sub = int(num_nodes / args.num_split)
 
         for j in range(args.num_split):
             if j != args.num_split - 1:
@@ -91,36 +82,38 @@ def train(data, X, Y, model, criterion, optim, batch_size):
             id = torch.tensor(id).to(device)
             tx = X[:, :, id, :]
             ty = Y[:, id]
-            output = model(tx,id)
-            ty = ty.unsqueeze(2)  # Change shape to [32, num_sub, 1]
-            ty = ty.expand(-1, -1, model._receptive_field) 
+
+            output = model(tx, idx = id)
             output = torch.squeeze(output)
-            #output = output.mean(dim=2) #change this method if needed to max or anything else 
-            scale = data.scale.expand(output.size(0), data.m)
-            scale = scale[:,id]
-            scale = scale.unsqueeze(2)  # Change shape from [32, 2] to [32, 2, 1]
-            scale = scale.expand(-1, -1, model._receptive_field)  
-            loss = criterion(output * scale, ty * scale)
+            if len(output.size()) > 2:
+                output = output.mean(dim = tuple(range(2, len(output.size()))))
+            output = Y_scaler.inverse_transform(output, id)
+
+            loss = criterion(output, ty)
             loss.backward()
             total_loss += loss.item()
             n_samples += (output.size(0) * data.m)
             grad_norm = optim.step()
 
-        if iter % 10 == 0:
-            print("iter:{:3d} | loss: {:.3f}".format(iter,loss.item() / (output.size(0) * data.m)))
-        
-        if iter > 201:
-            break
+        if iter % 100 == 0:
+            curr_time = time.time()
+            print("iter:{:3d} | loss: {:.3f} | run time: {:.3f}".format(iter,loss.item() / (output.size(0) * data.m), curr_time - track_time))
+            curr_time = track_time
 
         iter += 1
+
     return total_loss / n_samples
 
 
 def main(args):
+    global num_nodes
 
     print("Start loading data")
 
     Data = DataLoader(args.data_directory, args.time_interval, train = 0.6, valid = 0.2, device = device, horizon = args.horizon, window = args.seq_in_len, normalize = args.normalize, stationary_check = args.stationarity, noise_removal = args.noise_removal)
+    num_nodes = Data.m
+    os.makedirs(os.path.split(args.save)[0], exist_ok = True)
+
 
     print("Finish preprocessing data")
     print("Start loading model")
@@ -131,7 +124,7 @@ def main(args):
         layer_norm_affline = False
 
         # Missing kernel set and kernel size
-        model = MTGNN(args.gcn_true, args.buildA_true, args.gcn_depth, args.num_nodes,
+        model = MTGNN(args.gcn_true, args.buildA_true, args.gcn_depth, num_nodes,
                     kernel_set_pass, kernel_size, args.dropout, args.subgraph_size,
                     args.node_dim, args.dilation_exponential,args.conv_channels, args.residual_channels,
                     args.skip_channels, args.end_channels,
@@ -259,7 +252,6 @@ if __name__ == "__main__":
     parser.add_argument("--gcn_true", action = "store_true", default = True, help = "whether to add graph convolution layer")
     parser.add_argument("--buildA_true", action = "store_true", default = True, help = "whether to construct adaptive adjacency matrix")
     parser.add_argument("--gcn_depth", type = int, default = 2, help = "graph convolution depth")
-    parser.add_argument("--num_nodes", type = int, default = 137, help = "number of nodes/variables")
     parser.add_argument("--dropout", type = float, default = 0.3, help ="dropout rate")
     parser.add_argument("--subgraph_size", type = int, default = 20, help ="k")
     parser.add_argument("--node_dim", type = int, default = 40, help = "dim of nodes")